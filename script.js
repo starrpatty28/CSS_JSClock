73896484b0f97f756009e8b04a1e87c4f02548df
starrpatty28 <starrpatty28@gmail.com>
2017-02-21 17:46:31 -0800

    fix spacing of code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.mins-hand');
+const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
@@ -9,13 +9,13 @@ function setDate() {
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = 'rotates(${secondDegrees}deg)';
 
-const mins = now.getMins();
-const minsDegrees = ((mins / 60) * 360) + 90;
-minsHand.style.transform = 'rotates(${minsDegrees}deg)';
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = 'rotates(${minsDegrees}deg)';
 
-const hour = now.getHours();
-const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-hourHand.style.transform = 'rotate(${hourDegrees}deg)';
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = 'rotate(${hourDegrees}deg)';
 }
 
 setInterval(setDate, 1000);
